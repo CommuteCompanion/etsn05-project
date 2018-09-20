3c1be9e0b6ea91971f40ad63da21c1d9610ce80b
Jon Stålhammar <jost95@gmail.com>
2018-09-20 10:32:40 +0200

    Added support for sending SMTP emails
    Anyone can now send emails using the classes *MailHandler* and *CustomEmail*.

---
+++ src/main/java/se/stalhammar/mail/TestMail.java
@@ -0,0 +1,18 @@
+package se.stalhammar.mail;
+
+import org.simplejavamail.MailException;
+
+public class TestMail {
+
+	public static void main(String[] args) {
+		MailHandler mailHandler = new MailHandler();	
+		CustomEmail welcomeEmail = new CustomEmail("Jon", "Stålhammar", "jost95@gmail.com", CustomEmail.WELCOME_EMAIL);
+		
+		try {
+			mailHandler.sendMail(welcomeEmail.getEmail());
+		} catch (MailException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+
+}
