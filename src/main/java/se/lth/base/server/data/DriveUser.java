d5a7a050e23278b190aba3b538bfc84c52c4ef82
Jon Stålhammar <jost95@gmail.com>
2018-09-23 10:38:01 +0200

    Added drive user DTO

---
+++ src/main/java/se/lth/base/server/data/DriveUser.java
@@ -0,0 +1,40 @@
+package se.lth.base.server.data;
+
+public class DriveUser {
+	private final int driveId, userId;
+	private final String start, stop;
+	private final boolean driver, accepted;
+	
+	public DriveUser(int driveId, int userId, String start, String stop, boolean driver, boolean accepted) {
+		this.driveId = driveId;
+		this.userId = userId;
+		this.start = start;
+		this.stop = stop;
+		this.driver = driver;
+		this.accepted = accepted;
+	}
+	
+	public int getDriveId() {
+		return driveId;
+	}
+	
+	public int getUserId() {
+		return userId;
+	}
+	
+	public String getStart() {
+		return start;
+	}
+	
+	public String getStop() {
+		return stop;
+	}
+	
+	public boolean isDriver() {
+		return driver;
+	}
+	
+	public boolean isAccepted() {
+		return accepted;
+	}
+}
