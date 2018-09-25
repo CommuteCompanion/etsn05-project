964b1f29ae3f8e0b6a466e8e86ed1ae14fa8e4b3
Jon Stålhammar <jost95@gmail.com>
2018-09-25 20:21:05 +0200

    Fixed some endpoints paths in drive resource

--- src/main/java/se/lth/base/server/rest/DriveResource.java
+++ src/main/java/se/lth/base/server/rest/DriveResource.java
@@ -110,7 +110,7 @@ public class DriveResource {
     	throw new WebApplicationException("No available seats left", Status.PRECONDITION_FAILED);
     }
     
-    @Path("{driveId/user/{userId}")
+    @Path("{driveId}/user/{userId}")
     @PUT
     @RolesAllowed(Role.Names.USER)
     public DriveUser acceptUserInDrive(@PathParam("{driveId}") int driveId, @PathParam("{userId}") int userId) {
@@ -122,7 +122,7 @@ public class DriveResource {
     	throw new WebApplicationException("Only driver allowed to accept passengers", Status.UNAUTHORIZED);
     }
     
-    @Path("{driveId/user/{userId}")
+    @Path("{driveId}/user/{userId}")
     @DELETE
     @RolesAllowed(Role.Names.USER)
     public void removeUserFromDrive(@PathParam("{driveId}") int driveId, @PathParam("{userId}") int userId) {
@@ -132,7 +132,7 @@ public class DriveResource {
     	throw new WebApplicationException("Only driver allowed to remove passengers", Status.UNAUTHORIZED);
     }
     
-    @Path("{driveId/complete}")
+    @Path("{driveId}/complete")
     @PUT
     @RolesAllowed(Role.Names.USER)
     public Drive completeDrive(@PathParam("{driveId}") int driveId) {
@@ -144,7 +144,7 @@ public class DriveResource {
     	throw new WebApplicationException("Only driver allowed to accept passengers", Status.UNAUTHORIZED);
     }
     
-    @Path("{driveId/rate")
+    @Path("{driveId}/rate")
     @POST
     @RolesAllowed(Role.Names.USER)
     @Consumes(MediaType.APPLICATION_JSON + ";charset=utf-8")
