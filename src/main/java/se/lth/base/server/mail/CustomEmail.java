db80f86a180439c047070e24f61bb679c41f9e99
Jon Stålhammar <jost95@gmail.com>
2018-10-02 10:29:52 +0200

    Email functionality expanded

--- src/main/java/se/lth/base/server/mail/CustomEmail.java
+++ src/main/java/se/lth/base/server/mail/CustomEmail.java
@@ -2,64 +2,233 @@ package se.lth.base.server.mail;
 
 import org.simplejavamail.email.Email;
 import org.simplejavamail.email.EmailBuilder;
+import org.simplejavamail.email.Recipient;
+import org.slf4j.LoggerFactory;
+import se.lth.base.server.data.DriveWrap;
+import se.lth.base.server.data.User;
+
+import javax.mail.Message;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
- * Creates a SimpleEmail object based on a standard predefined email type
- * 
- * @author Group 1 ETSN05 2018
+ * Creates a SimpleEmail object based on a standard predefined EmailType
  *
+ * @author Group 1 ETSN05 2018
  */
 public class CustomEmail {
-	public static final int WELCOME_EMAIL = 1;
-	
-	private String firstName;
-	private String lastName;
-	private String email;
-	private int emailType;
+    private final DriveWrap driveWrap;
+    private final EmailType emailType;
+    private List<Recipient> recipients;
 
-	public CustomEmail(String firstName, String lastName, String email, int emailType) {
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.email = email;
-		this.emailType = emailType;
-	}
-	
-	public Email getEmail() {
-		return EmailBuilder.startingBlank()
-				.from(MailHandler.DEFAULT_SENDER)
-				.to(firstName + " " + lastName, email)
-				.withSubject(getSubject())
-				.withHTMLText(getMessage())
-		        .withPlainText("Please view this email in a modern email client!")
-		        .buildEmail(); 	
-	}
-	
-	public String getSubject() {		
-		switch (emailType) {
-		case WELCOME_EMAIL: return  getWelcomeSubject();
-		default: return "INVALID EMAIL TYPE";
-		}
-	}
-	
-	public String getMessage() {
-		switch (emailType) {
-		case WELCOME_EMAIL: return  getWelcomeMessage();
-		default: return "INVALID EMAIL TYPE";
-		}
-	}
-	
-	// ---------- STANDARD EMAILS BELOW --------------
-	
-	private String getWelcomeSubject() {
-		return "Welcome to CommuteCompanion " + firstName;
-	}
-	
-	private String getWelcomeMessage() {
-		StringBuilder sb = new StringBuilder();
-		
-		sb.append("<h1>Hi ").append(firstName).append("</h1>");
-		sb.append("<p>Welcome to CommuteCompanion!</p>");
-		
-		return sb.toString();
-	}
+    /**
+     * Basic custom email, handles most scenarios
+     *
+     * @param driveWrap DriveWrap DTO
+     * @param emailType EmailType defined by se.lth.base.server.mail.EmailType
+     */
+    public CustomEmail(DriveWrap driveWrap, EmailType emailType) {
+        this(driveWrap, emailType, null);
+    }
+
+    /**
+     * Used to send non drive related emails
+     *
+     * @param user User to be warned
+     * @param emailType EmailType defined by se.lth.base.server.mail.EmailType
+     */
+    public CustomEmail(User user, EmailType emailType) {
+        this(null, emailType, user);
+    }
+
+    // TODO: Additional constructor to handle search filter match and adjust master constructor
+    public CustomEmail(DriveWrap driveWrap, EmailType emailType, User user) {
+        this.driveWrap = driveWrap;
+        this.emailType = emailType;
+        this.recipients = new ArrayList<>();
+
+        if (user != null) {
+            // TODO: Add user attributes when implemented
+            recipients.add(new Recipient("name", "email", Message.RecipientType.TO));
+        }
+    }
+
+
+    public Email getEmail() {
+        Map<String, String> subjectAndBody = getSubjectAndBody();
+
+        return EmailBuilder.startingBlank()
+                .from(MailHandler.DEFAULT_SENDER)
+                .to(recipients)
+                .withSubject(subjectAndBody.get("subject"))
+                .withHTMLText(subjectAndBody.get("body"))
+                .withPlainText("Please view this email in a modern email client!")
+                .buildEmail();
+    }
+
+    public Map<String, String> getSubjectAndBody() {
+        String subject = "", body = "";
+        Map<String, String> subjectAndBody = new HashMap<>();
+
+        switch (emailType) {
+            case WELCOME: {
+                subject = getWelcomeSubject();
+                body = getWelcomeBody();
+                break;
+            }
+            case NEW_PASSENGER_ON_TRIP: {
+                subject = getNewPassengerOnTripSubject();
+                body = getNewPassengerOnTripBody();
+            }
+            case BOOKING_CONFIRMED: {
+                subject = getBookingConfirmedSubject();
+                body = getBookingConfirmedBody();
+                break;
+            }
+            case RATING: {
+                subject = getRatingSubject();
+                body = getRatingBody();
+                break;
+            }
+            case FILTER_MATCH: {
+                subject = getFilterMatchSubject();
+                body = getFilterMatchBody();
+                break;
+            }
+            case PASSENGER_CANCELLED_TRIP: {
+                subject = getPassengerCancelledTripSubject();
+                body = getPassengerCancelledTripBody();
+                break;
+            }
+            case DRIVER_CANCELLED_DRIVE: {
+                subject = getDriverCancelledDriveSubject();
+                body = getDriverCancelledDriveBody();
+                break;
+            }
+            case WARNING: {
+                subject = getWarningSubject();
+                body = getWarningBody();
+                break;
+            }
+            case DRIVER_REMOVED_PASSENGER: {
+                subject = getDriverRemovedPassengerSubject();
+                body = getDriverRemovedPassengerBody();
+                break;
+            }
+        }
+
+        subjectAndBody.put("subject", subject);
+        subjectAndBody.put("body", body);
+        return subjectAndBody;
+    }
+
+    // Helper methods fo tech HTML email template
+    private String getResource(String resourceName) {
+        InputStream inputStream = CustomEmail.class.getResourceAsStream(resourceName);
+        InputStreamReader streamReader = new InputStreamReader(inputStream);
+
+
+        try (BufferedReader br = new BufferedReader(streamReader)) {
+            return br.lines().collect(Collectors.joining(System.lineSeparator()));
+        } catch (IOException e) {
+            LoggerFactory.getLogger(CustomEmail.class).error(e.getMessage(), e);
+        }
+
+        return "Error: could not read email template";
+    }
+
+    private String parseResource(String emailTemplate, Map<String, String> replacements) {
+        for (Map.Entry<String, String> e : replacements.entrySet()) {
+            emailTemplate = emailTemplate.replaceAll("{{" + e.getKey() + "}}", e.getValue());
+        }
+
+        return emailTemplate;
+    }
+
+    private String getButton(String link, String text) {
+        Map<String, String> buttonReplacements = new HashMap<>();
+        buttonReplacements.put("buttonLink", link);
+        buttonReplacements.put("buttonText", text);
+        return parseResource(getResource("button-template.html"), buttonReplacements);
+    }
+
+    // ---------- STANDARD EMAILS BELOW --------------
+    private String getWelcomeSubject() {
+        return "";
+    }
+
+    private String getWelcomeBody() {
+        return "";
+    }
+
+    private String getNewPassengerOnTripSubject() {
+        return "";
+    }
+
+    private String getNewPassengerOnTripBody() {
+        return "";
+    }
+
+    private String getBookingConfirmedSubject() {
+        return "";
+    }
+
+    private String getBookingConfirmedBody() {
+        return "";
+    }
+
+    private String getRatingSubject() {
+        return "";
+    }
+
+    private String getRatingBody() {
+        return "";
+    }
+
+    private String getFilterMatchSubject() {
+        return "";
+    }
+
+    private String getFilterMatchBody() {
+        return "";
+    }
+
+    private String getPassengerCancelledTripSubject() {
+        return "";
+    }
+
+    private String getPassengerCancelledTripBody() {
+        return "";
+    }
+
+    private String getDriverCancelledDriveSubject() {
+        return "";
+    }
+
+    private String getDriverCancelledDriveBody() {
+        return "";
+    }
+
+    private String getWarningSubject() {
+        return "";
+    }
+
+    private String getWarningBody() {
+        return "";
+    }
+
+    private String getDriverRemovedPassengerSubject() {
+        return "";
+    }
+
+    private String getDriverRemovedPassengerBody() {
+        return "";
+    }
 }
--- src/main/java/se/lth/base/server/mail/MailHandler.java
+++ src/main/java/se/lth/base/server/mail/MailHandler.java
@@ -5,6 +5,11 @@ import org.simplejavamail.email.Email;
 import org.simplejavamail.mailer.Mailer;
 import org.simplejavamail.mailer.MailerBuilder;
 import org.simplejavamail.mailer.config.TransportStrategy;
+import se.lth.base.server.data.Drive;
+import se.lth.base.server.data.DriveWrap;
+import se.lth.base.server.data.User;
+
+import java.util.List;
 
 /**
  * Wrapper class for SimpleMail mail handler
@@ -33,7 +38,86 @@ public class MailHandler {
 				.buildMailer();
 	}
 	
-	public void sendMail(Email email) throws MailException {
+	private void sendMail(Email email) throws MailException {
 		mailer.sendMail(email);
 	}
+
+    /**
+     * Send an email to the driver for the last passenger added
+     *
+     * @param user User to welcome
+     */
+    public void welcomeUser(User user) {
+        sendMail(new CustomEmail(user,EmailType.WELCOME).getEmail());
+    }
+
+    /**
+     * Send an email to the driver for the last passenger added
+     *
+     * @param driveWrap DriveWrap DTO object
+     */
+	public void notifyDriverNewPassangerOnTrip(DriveWrap driveWrap) {
+	    sendMail(new CustomEmail(driveWrap,EmailType.NEW_PASSENGER_ON_TRIP).getEmail());
+    }
+
+    /**
+     * Send an email to the last passenger with accepted option set to TRUE
+     *
+     * @param driveWrap DriveWrap DTO object
+     */
+    public void notifyPassengerBookingConfirmed(DriveWrap driveWrap) {
+        sendMail(new CustomEmail(driveWrap,EmailType.BOOKING_CONFIRMED).getEmail());
+    }
+
+    /**
+     * Sends an email to all users in drive to rate the drive
+     *
+     * @param driveWrap
+     */
+    public void sendLinkToRatingScreen(DriveWrap driveWrap) {
+        sendMail(new CustomEmail(driveWrap,EmailType.RATING).getEmail());
+    }
+
+    // TODO: Uncomment when SearchFilter DTO implemented
+    /*public void notifyUserSearchFilterMatch(DriveWrap driveWrap, SearchFilter searchFilter) {
+        sendMail(new CustomEmail(driveWrap, CustomEmail.FILTER_MATCH_EMAIL, searchFilter).getEmail());
+    }*/
+
+    /**
+     * Sends an email to a driver that passenger has cancelled trip
+     *
+     * @param driveWrap DriveWrap DTO
+     * @param passenger Passenger that has been removed from drive
+     */
+    public void notifyDriverPassengerCancelledTrip(DriveWrap driveWrap, User passenger) {
+        sendMail(new CustomEmail(driveWrap, EmailType.PASSENGER_CANCELLED_TRIP, passenger).getEmail());
+    }
+
+    /**
+     * Sends an email to passenger that the driver has removed him/her
+     *
+     * @param driveWrap DriveWrap DTO
+     * @param passenger Passenger that has been removed from drive
+     */
+    public void notifyPassengerDriverRemovedPassenger(DriveWrap driveWrap, User passenger) {
+        sendMail(new CustomEmail(driveWrap, EmailType.DRIVER_REMOVED_PASSENGER, passenger).getEmail());
+    }
+
+    /**
+     * Sends an email to all passengers that the drive is cancelled
+     *
+     * @param driveWrap DriveWrap DTO
+     */
+    public void notifyPassengersDriverCancelledDrive(DriveWrap driveWrap) {
+        sendMail(new CustomEmail(driveWrap, EmailType.DRIVER_CANCELLED_DRIVE).getEmail());
+    }
+
+    /**
+     * Sends an email to a user that he has been warned by an admin
+     *
+     * @param user User to be warned
+     */
+    public void notifyUserHasBeenWarned(User user) {
+        sendMail(new CustomEmail(user, EmailType.WARNING).getEmail());
+    }
 }
