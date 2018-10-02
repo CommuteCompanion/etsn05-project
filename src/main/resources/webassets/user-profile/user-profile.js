ae818d70b2b6a04d06a16480115fce9d9826914c
Mathias Kindberg <mathias.kindberg@gmail.com>
2018-10-02 22:27:35 +0200

    Finalized basic user settings page.

--- src/main/resources/webassets/user-profile/user-profile.js
+++ src/main/resources/webassets/user-profile/user-profile.js
@@ -5,31 +5,54 @@ base.userProfileController = (function() {
     };
     var view = {
         render: function() {
-            document.getElementById("set-username").value = model.user.username;
+            base.rest.getUser().then(function(u){
+                model.user = u;
+                return u;
+            }).then(function() {
+                document.getElementById('set-username').value = model.user.username;
+            });  
+        },
+        resetEdit: function() {
+            view.render();
+            document.getElementById('set-password').value = '';
+            document.getElementById('set-password-confirm').value = '';
         }
     };
     var controller = {
         deleteUser: function() {
-            console.log(model.user.id);
-            console.log(model.user.username);
-            console.log(model.user);
             base.rest.deleteUser(model.user.id).then(function() {
                 base.rest.logout();
             });
         },
         submitUser: function(submitEvent) {
-            console.log(model.user);
+            submitEvent.preventDefault;
+            var username = document.getElementById('set-username').value;
+            var password = document.getElementById('set-password').value;
+            var repeatPassword = document.getElementById('set-password-confirm').value;
+            var id = model.user.id;
+            var role = model.user.role.name;
+            credentials = {username, password, role};
+            if (password === '') {
+                delete credentials.password;
+            }
+            if (password === repeatPassword) {
+                base.rest.putUser(id, credentials).then(function(user) {
+                    if (user.error) {
+                        alert(user.message);
+                    } else {
+                        view.render();
+                    }
+                });
+            } else {
+                alert('Passwords don\'t match');
+            }
+            return false;
         },
         load: function() {
             document.getElementById('user-form').onsubmit = controller.submitUser;
+            document.querySelector('#reset-user').onclick = view.resetEdit;
             document.querySelector('#delete-account').onclick = controller.deleteUser;
-
-            base.rest.getUser().then(function(u){
-                model.user = u;
-                return u;
-            }).then(function() {
-                view.render();
-            });  
+            view.render();
         },
     };
 
