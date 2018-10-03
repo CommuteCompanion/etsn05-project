37083ce1823bc878d15281b90370d4181d95a4c3
Jon Stålhammar <jost95@gmail.com>
2018-10-03 19:51:30 +0200

    Fixed error in sql statement

--- src/main/resources/se/lth/base/server/database/schema.sql
+++ src/main/resources/se/lth/base/server/database/schema.sql
@@ -48,11 +48,11 @@ CREATE TABLE drive(drive_id INT AUTO_INCREMENT,
 
 CREATE TABLE drive_milestone(milestone_id INT AUTO_INCREMENT,
                              drive_id INT NOT NULL,
-                             milestone VARCHAR(255) NOT NULL,
-                             order TINYINT NOT NULL,
+                             milestone_name VARCHAR(255) NOT NULL,
+                             milestone_order TINYINT NOT NULL,
                              departure_time TIMESTAMP NOT NULL,
                              PRIMARY KEY(milestone_id),
-                             FOREIGN KEY(drive_id) REFERENCES drive(drive_id) ON DELETE CASCADE);
+                             );
 
 CREATE TABLE drive_user(drive_id INT NOT NULL,
                         user_id INT NOT NULL,
@@ -72,7 +72,7 @@ CREATE TABLE drive_report(report_id INT AUTO_INCREMENT,
                           FOREIGN KEY(drive_id) REFERENCES drive(drive_id) ON DELETE CASCADE,
                           FOREIGN KEY(reported_by_user_id) REFERENCES user(user_id) ON DELETE CASCADE);
 
-CREATE TABLE search_filter(search_filter_id INT AUTO_INCREMENT
+CREATE TABLE search_filter(search_filter_id INT AUTO_INCREMENT,
                            user_id INT NOT NULL,
                            start VARCHAR(255),
                            stop VARCHAR(255),
