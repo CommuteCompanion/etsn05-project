e01c222015bfa840076b54e2b0f08f5aeb215726
Hackerlacka <robin.robbe96@gmail.com>
2018-10-04 19:24:58 +0200

    Renamed 'milestone'->'milestone_name' for some queries. Removed 'order' from query. Added missing 'departure_time' from queries.
    Fixes #60 Fixes #61

--- src/main/java/se/lth/base/server/data/DriveMilestoneDataAccess.java
+++ src/main/java/se/lth/base/server/data/DriveMilestoneDataAccess.java
@@ -16,37 +16,37 @@ import java.util.List;
  */
 public class DriveMilestoneDataAccess extends DataAccess<DriveMilestone> {
 
-	private static final class MilestoneMapper implements Mapper<DriveMilestone> {
-		@Override
-		public DriveMilestone map(ResultSet resultSet) throws SQLException {
-			return new DriveMilestone(resultSet.getInt("milestone_id"),
-					resultSet.getInt("drive_id"),
-					resultSet.getString("milestone"),
-					resultSet.getTimestamp("departure_time"));
-		}
-	}
-
-	public DriveMilestoneDataAccess(String driverUrl) {
-		super(driverUrl, new MilestoneMapper());
-	}
-
 	public DriveMilestone addMilestone(int driveId, String milestone, Timestamp departureTime) {
-		int milestoneId = insert("INSERT INTO drive_milestone (drive_id, order, stop, departure_time) VALUES(?,?,?,?)");
+        int milestoneId = insert("INSERT INTO drive_milestone (drive_id, milestone_name, departure_time) VALUES(?,?,?)");
 		return new DriveMilestone(milestoneId, driveId, milestone, departureTime);
 	}
 
+    public DriveMilestoneDataAccess(String driverUrl) {
+        super(driverUrl, new MilestoneMapper());
+    }
+
 	public DriveMilestone updateMilestone(int milestoneId, String milestone) {
-		execute("UPDATE drive_milestone SET milestone = ? WHERE milestone_id = ?", milestone, milestoneId);
+        execute("UPDATE drive_milestone SET milestone_name = ? WHERE milestone_id = ?", milestone, milestoneId);
 		return getMilestone(milestoneId);
 	}
 
 	public DriveMilestone getMilestone(int milestoneId) {
-		return queryFirst("SELECT milestone_id, drive_id, milestone FROM drive_milestone WHERE milestone_id = ?", milestoneId);
+        return queryFirst("SELECT milestone_id, drive_id, milestone_name, departure_time FROM drive_milestone WHERE milestone_id = ?", milestoneId);
 	}
 
 	public List<DriveMilestone> getMilestonesForDrive(int driveId) {
-		return query("SELECT milestone_id, drive_id, milestone FROM drive_milestone WHERE drive_id = ?", driveId);
-	}
+        return query("SELECT milestone_id, drive_id, milestone_name, departure_time FROM drive_milestone WHERE drive_id = ?", driveId);
+    }
+
+    private static final class MilestoneMapper implements Mapper<DriveMilestone> {
+        @Override
+        public DriveMilestone map(ResultSet resultSet) throws SQLException {
+            return new DriveMilestone(resultSet.getInt("milestone_id"),
+                    resultSet.getInt("drive_id"),
+                    resultSet.getString("milestone_name"),
+                    resultSet.getTimestamp("departure_time"));
+        }
+    }
 
 	public boolean deleteMilestone(int milestoneId) {
 		return execute("DELETE FROM drive_milestone WHERE milestone_id = ?", milestoneId) > 0;
