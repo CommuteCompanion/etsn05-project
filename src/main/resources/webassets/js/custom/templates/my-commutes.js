4c00a3d21761b9133c70b553b1104087eea759d6
Arvid <arvid.pilhall@gmail.com>
2018-10-09 15:53:48 +0200

    My commutes page frontend almost finished

--- src/main/resources/webassets/js/custom/templates/my-commutes.js
+++ src/main/resources/webassets/js/custom/templates/my-commutes.js
@@ -1,10 +1,41 @@
 window.base = window.base || {};
 
 window.base.myCommutesController = (() => {
+    var tabToRemove = 'drive-container';
+    var previousBtn = 'drive-btn';
     const controller = {
         load: () => {
+            document.getElementById(previousBtn).style.background = '#fff'
+            document.getElementById('trip-container').style.display = 'none';
+            document.getElementById('archive-container').style.display = 'none';
 
-           
+            document.getElementById('drive-btn').onclick = function() {  
+                document.getElementById(previousBtn).style.background = '#A8D0E6';
+                previousBtn = 'drive-btn';
+                document.getElementById(tabToRemove).style.display = 'none';
+                document.getElementById('drive-container').style.display = 'block';
+                document.getElementById(previousBtn).style.background = '#fff'
+                tabToRemove = 'drive-container';
+            };
+            document.getElementById('trip-btn').onclick = function() {
+                document.getElementById(previousBtn).style.background = '#A8D0E6';
+                previousBtn = 'trip-btn';
+                document.getElementById(tabToRemove).style.display = 'none';
+                document.getElementById('trip-container').style.display = 'block';
+                document.getElementById(previousBtn).style.background = '#fff'
+                tabToRemove = 'trip-container';
+            };
+            document.getElementById('archive-btn').onclick = function() {
+                document.getElementById(previousBtn).style.background = '#A8D0E6';
+                previousBtn = 'archive-btn';
+                document.getElementById(tabToRemove).style.display = 'none';    
+                document.getElementById('archive-container').style.display = 'block';
+                document.getElementById(previousBtn).style.background = '#fff'
+                tabToRemove = 'archive-container';
+            };
+        },
+        tabViews: () => {
+
         }
     };
 
