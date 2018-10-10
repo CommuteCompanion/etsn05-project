78f12e1d30cb94949b64dfb7e29903efd9d732e8
Arvid <arvid.pilhall@gmail.com>
2018-10-10 21:53:46 +0200

    FIxed PR comments

--- src/main/resources/webassets/js/custom/templates/my-commutes.js
+++ src/main/resources/webassets/js/custom/templates/my-commutes.js
@@ -4,14 +4,13 @@ window.base.myCommutesController = (() => {
     var tabToRemove = 'drive-container';
     var previousBtn = 'drive-btn';
 
-    var rateModalElement = document.getElementById('rateModal');
+    var rateModalElement = document.getElementById('modal');
     var rateModal = new Modal(rateModalElement,
                               { 
         content: '<div class="modal-body">Give it a rate!<div class="container"><div class="row"><div class="rating"><input type="button" id="star5" name="rating" value="5" /><label for="star5" title="Meh">5 stars</label><input type="button" id="star4" name="rating" value="4" /><label for="star4" title="Kinda bad">4 stars</label><input type="button" id="star3" name="rating" value="3" /><label for="star3" title="Kinda bad">3 stars</label><input type="button" id="star2" name="rating" value="2" /><label for="star2" title="Sucks big tim">2 stars</label><input type="button" id="star1" name="rating" value="1" /><label for="star1" title="Sucks big time">1 star</label></div></div></div></div>', // sets modal content
     });
     const controller = {
         load: () => {
-            document.getElementById(previousBtn).style.background = '#fff'
             document.getElementById('trip-container').style.display = 'none';
             document.getElementById('archive-container').style.display = 'none';
 
@@ -20,27 +19,21 @@ window.base.myCommutesController = (() => {
             };
 
             document.getElementById('drive-btn').onclick = () => {  
-                document.getElementById(previousBtn).style.background = '#A8D0E6';
                 previousBtn = 'drive-btn';
                 document.getElementById(tabToRemove).style.display = 'none';
                 document.getElementById('drive-container').style.display = 'block';
-                document.getElementById(previousBtn).style.background = '#fff'
                 tabToRemove = 'drive-container';
             };
             document.getElementById('trip-btn').onclick = () => {
-                document.getElementById(previousBtn).style.background = '#A8D0E6';
                 previousBtn = 'trip-btn';
                 document.getElementById(tabToRemove).style.display = 'none';
                 document.getElementById('trip-container').style.display = 'block';
-                document.getElementById(previousBtn).style.background = '#fff'
                 tabToRemove = 'trip-container';
             };
             document.getElementById('archive-btn').onclick = () => {
-                document.getElementById(previousBtn).style.background = '#A8D0E6';
                 previousBtn = 'archive-btn';
                 document.getElementById(tabToRemove).style.display = 'none';    
                 document.getElementById('archive-container').style.display = 'block';
-                document.getElementById(previousBtn).style.background = '#fff'
                 tabToRemove = 'archive-container';
             };
             document.getElementById('drive-rate-modalTrigger').onclick = controller.showRateModal;
