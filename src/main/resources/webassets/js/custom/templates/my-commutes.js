7ee468daed6eddd912680b3aed5064db8ef8331a
Arvid <arvid.pilhall@gmail.com>
2018-10-10 15:38:40 +0200

    Added modals

--- src/main/resources/webassets/js/custom/templates/my-commutes.js
+++ src/main/resources/webassets/js/custom/templates/my-commutes.js
@@ -3,6 +3,18 @@ window.base = window.base || {};
 window.base.myCommutesController = (() => {
     var tabToRemove = 'drive-container';
     var previousBtn = 'drive-btn';
+
+    var rateModalElement = document.getElementById('rateModal');
+    var editModalElement = document.getElementById('editModal');
+    var rateModal = new Modal(rateModalElement,
+                          { 
+        content: '<div class="modal-body">Give it a rate!<div class="container"><div class="row"><div class="rating"><input type="button" id="star5" name="rating" value="5" /><label for="star5" title="Meh">5 stars</label><input type="button" id="star4" name="rating" value="4" /><label for="star4" title="Kinda bad">4 stars</label><input type="button" id="star3" name="rating" value="3" /><label for="star3" title="Kinda bad">3 stars</label><input type="button" id="star2" name="rating" value="2" /><label for="star2" title="Sucks big tim">2 stars</label><input type="button" id="star1" name="rating" value="1" /><label for="star1" title="Sucks big time">1 star</label></div></div></div></div>', // sets modal content
+    });
+    
+    var editModal = new Modal(editModalElement,
+                              {
+        content: '<div class="modal-body">Edit</div>',
+    });
     const controller = {
         load: () => {
             document.getElementById(previousBtn).style.background = '#fff'
@@ -33,9 +45,33 @@ window.base.myCommutesController = (() => {
                 document.getElementById(previousBtn).style.background = '#fff'
                 tabToRemove = 'archive-container';
             };
-        },
-        tabViews: () => {
 
+            document.getElementById('drive-edit-modalTrigger').onclick = controller.showModal;
+
+            document.getElementById('drive-rate-modalTrigger').onclick = controller.showRateModal;
+
+            document.getElementById('trip-edit-modalTrigger').onclick = controller.showModal;
+            
+            document.getElementById('trip-rate-modalTrigger').onclick = controller.showRateModal;
+            
+            document.getElementById('star1').onclick = controller.hideRateModal;
+            document.getElementById('star2').onclick = controller.hideRateModal;
+            document.getElementById('star3').onclick = controller.hideRateModal;
+            document.getElementById('star4').onclick = controller.hideRateModal;
+            document.getElementById('star5').onclick = controller.hideRateModal;
+            
+        },
+        showModal: () => {
+            editModal.show();
+        },
+        showRateModal: () => {
+            rateModal.show();
+        },
+        hideEditModal: () => {
+            editModal.hide();
+        },
+        hideRateModal: () => {
+            rateModal.hide();
         }
     };
 
