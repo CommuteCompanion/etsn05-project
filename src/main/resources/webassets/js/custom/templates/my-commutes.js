3779af677fc46877a55abc55f2425101ee488e41
Arvid <arvid.pilhall@gmail.com>
2018-10-11 19:51:39 +0200

    Added dynamic modals

--- src/main/resources/webassets/js/custom/templates/my-commutes.js
+++ src/main/resources/webassets/js/custom/templates/my-commutes.js
@@ -6,16 +6,9 @@ window.base.myCommutesController = (() => {
 
     const view = {
         loadRequestModal: driveId => {
-            console.log("hej");
-            var modal = new Modal(document.getElementById('main-modal'), {
+            new Modal(document.getElementById('main-modal'), {
                 content: '<div class="modal-body" id="' + driveId + '">Give it a rate!<div class="container"><div class="row"><div class="rating"><input type="button" id="star5" name="rating" value="5" /><label for="star5" title="Meh">5 stars</label><input type="button" id="star4" name="rating" value="4" /><label for="star4" title="Kinda bad">4 stars</label><input type="button" id="star3" name="rating" value="3" /><label for="star3" title="Kinda bad">3 stars</label><input type="button" id="star2" name="rating" value="2" /><label for="star2" title="Sucks big tim">2 stars</label><input type="button" id="star1" name="rating" value="1" /><label for="star1" title="Sucks big time">1 star</label></div></div></div></div>',
                 keyboard: false}).show()
-
-            document.getElementById('star1' + driveId).onclick = modal.hide();
-            document.getElementById('star2' + driveId).onclick = modal.hide();
-            document.getElementById('star3' + driveId).onclick = modal.hide();
-            document.getElementById('star4' + driveId).onclick = modal.hide();
-            document.getElementById('star5' + driveId).onclick = modal.hide();
         }
 
     };
@@ -26,23 +19,16 @@ window.base.myCommutesController = (() => {
             document.getElementById('archive-container').style.display = 'none';
 
             const requestModalTriggers = document.getElementsByClassName('mycom-past-btn');
-            console.log(requestModalTriggers[0].closest('.card').getAttribute('id').split('-')[1]);
             for (let i = 0; i < requestModalTriggers.length; i++) {
                 let trigger = requestModalTriggers[i];
-                console.log(trigger);
                 trigger.onclick = () => {
                     let driveId = trigger.closest('.card').getAttribute('id').split('-')[1];
                     view.loadRequestModal(driveId);
-                    console.log("hejdå");
                 };
-
-
             }
-
             document.getElementById('edit-btn').onclick = () => {
                 window.base.changeLocation('/#/create-drive.html');
             };
-
             document.getElementById('drive-btn').onclick = () => {  
                 previousBtn = 'drive-btn';
                 document.getElementById(tabToRemove).style.display = 'none';
@@ -63,6 +49,5 @@ window.base.myCommutesController = (() => {
             };
         }
     };
-
     return controller;
 });
