508e388333668318fcc966c04fed7a794492558f
Arvid <arvid.pilhall@gmail.com>
2018-10-11 19:42:52 +0200

    Started adding dynamic modal function

--- src/main/resources/webassets/js/custom/templates/my-commutes.js
+++ src/main/resources/webassets/js/custom/templates/my-commutes.js
@@ -4,15 +4,40 @@ window.base.myCommutesController = (() => {
     var tabToRemove = 'drive-container';
     var previousBtn = 'drive-btn';
 
-    var rateModalElement = document.getElementById('modal');
-    var rateModal = new Modal(rateModalElement,
-                              { 
-        content: '<div class="modal-body">Give it a rate!<div class="container"><div class="row"><div class="rating"><input type="button" id="star5" name="rating" value="5" /><label for="star5" title="Meh">5 stars</label><input type="button" id="star4" name="rating" value="4" /><label for="star4" title="Kinda bad">4 stars</label><input type="button" id="star3" name="rating" value="3" /><label for="star3" title="Kinda bad">3 stars</label><input type="button" id="star2" name="rating" value="2" /><label for="star2" title="Sucks big tim">2 stars</label><input type="button" id="star1" name="rating" value="1" /><label for="star1" title="Sucks big time">1 star</label></div></div></div></div>', // sets modal content
-    });
+    const view = {
+        loadRequestModal: driveId => {
+            console.log("hej");
+            var modal = new Modal(document.getElementById('main-modal'), {
+                content: '<div class="modal-body" id="' + driveId + '">Give it a rate!<div class="container"><div class="row"><div class="rating"><input type="button" id="star5" name="rating" value="5" /><label for="star5" title="Meh">5 stars</label><input type="button" id="star4" name="rating" value="4" /><label for="star4" title="Kinda bad">4 stars</label><input type="button" id="star3" name="rating" value="3" /><label for="star3" title="Kinda bad">3 stars</label><input type="button" id="star2" name="rating" value="2" /><label for="star2" title="Sucks big tim">2 stars</label><input type="button" id="star1" name="rating" value="1" /><label for="star1" title="Sucks big time">1 star</label></div></div></div></div>',
+                keyboard: false}).show()
+
+            document.getElementById('star1' + driveId).onclick = modal.hide();
+            document.getElementById('star2' + driveId).onclick = modal.hide();
+            document.getElementById('star3' + driveId).onclick = modal.hide();
+            document.getElementById('star4' + driveId).onclick = modal.hide();
+            document.getElementById('star5' + driveId).onclick = modal.hide();
+        }
+
+    };
     const controller = {
+        view,
         load: () => {
             document.getElementById('trip-container').style.display = 'none';
             document.getElementById('archive-container').style.display = 'none';
+
+            const requestModalTriggers = document.getElementsByClassName('mycom-past-btn');
+            console.log(requestModalTriggers[0].closest('.card').getAttribute('id').split('-')[1]);
+            for (let i = 0; i < requestModalTriggers.length; i++) {
+                let trigger = requestModalTriggers[i];
+                console.log(trigger);
+                trigger.onclick = () => {
+                    let driveId = trigger.closest('.card').getAttribute('id').split('-')[1];
+                    view.loadRequestModal(driveId);
+                    console.log("hejdå");
+                };
+
+
+            }
 
             document.getElementById('edit-btn').onclick = () => {
                 window.base.changeLocation('/#/create-drive.html');
@@ -36,22 +61,6 @@ window.base.myCommutesController = (() => {
                 document.getElementById('archive-container').style.display = 'block';
                 tabToRemove = 'archive-container';
             };
-            document.getElementById('drive-rate-modalTrigger').onclick = controller.showRateModal;
-
-            document.getElementById('trip-rate-modalTrigger').onclick = controller.showRateModal;
-
-            document.getElementById('star1').onclick = controller.hideRateModal;
-            document.getElementById('star2').onclick = controller.hideRateModal;
-            document.getElementById('star3').onclick = controller.hideRateModal;
-            document.getElementById('star4').onclick = controller.hideRateModal;
-            document.getElementById('star5').onclick = controller.hideRateModal;
-
-        },
-        showRateModal: () => {
-            rateModal.show();
-        },
-        hideRateModal: () => {
-            rateModal.hide();
         }
     };
 
