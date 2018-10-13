6c4ff561d453f015a6c6e73ecdcbb5de69216dce
Arvid <arvid.pilhall@gmail.com>
2018-10-13 10:51:00 +0200

    Connected front to back

--- src/main/resources/webassets/js/custom/templates/user-profile.js
+++ src/main/resources/webassets/js/custom/templates/user-profile.js
@@ -7,8 +7,8 @@ window.base.userProfileController = (() => {
 
     const view = {
         render: () => window.base.rest.getUser().then(u => {
-                model.user = u;
-                return u;
+            model.user = u;
+            return u;
         }).then(() => document.getElementById('set-email').value = model.user.email),
         clearChanges: () => {
             view.render();
@@ -18,21 +18,34 @@ window.base.userProfileController = (() => {
     };
     const controller = {
         deleteUser: () => window.base.rest.deleteUser(model.user.userId)
-                .then(window.base.rest.logout())
-                .then(() => window.location.replace('/')),
+        .then(window.base.rest.logout())
+        .then(() => window.location.replace('/')),
         submitUser: submitEvent => {
             submitEvent.preventDefault();
             const email = document.getElementById('set-email').value;
+            const firstName = document.getElementById('set-firstname').value;
+            const lastName = document.getElementById('set-lastname').value;
+            if(document.getElementById('set-male').checked) {
+                model.user.gender = 0;
+            } else if (document.getElementById('set-female').checked) {
+                model.user.gender = 1;
+            };
+            if(document.getElementById('set-license-true').checked) {
+                model.user.license = true;
+            } else if (document.getElementById('set-female').checked) {
+                model.user.license = false;
+            };
             const password = document.getElementById('set-password').value;
             const repeatPassword = document.getElementById('set-password-confirm').value;
             const id = model.user.userId;
             const role = model.user.role.name;
             const credentials = {email, password, role};
+            const user = model.user;
             if (password === '') {
                 delete credentials.password;
             }
             if (password === repeatPassword) {
-                window.base.rest.putUser(id, {email, password, role}).then(user => {
+                window.base.rest.putUser(id, {email, password, role, user}).then(user => {
                     if (user.error) {
                         alert(user.message);
                     } else {
