61e19578578e55aa04d37d175aecd19941bf9375
Jon Stålhammar <jost95@gmail.com>
2018-10-14 14:39:33 +0200

    Changed HTML style in JS

--- src/main/resources/webassets/js/custom/templates/search.js
+++ src/main/resources/webassets/js/custom/templates/search.js
@@ -77,7 +77,85 @@ window.base.searchController = (() => {
                 }
 
                 // language=HTML
-                searchResults += `<div class="row border bg-white shadow-sm">\n            <div class="col-2 border-right">\n                <h5 class="mt-3 mb-0 text-muted font-weight-bold">${driverFirstName}</h5>\n                <p class="text-muted">${driverGender}, ${driverAge}</p>\n                <div class="row mt-4">\n                    <div class="col-2">\n                        <p class="mb-0 text-muted"><i class="fas fa-car fa-sm"></i></p>\n                        <p class="text-muted mb-0"><i class="fas fa-star fa-sm"></i></p>\n                        <p class="text-muted"><i class="fas fa-chart-bar fa-sm"></i></p>\n                    </div>\n                    <div class="col-9">\n                        <p class="text-muted mb-0">${driverNumberDriven} driven</p>\n                        <p class="text-muted mb-0">${driverRating} rating</p>\n                        <p class="text-muted">${driverReviews} reviews</p>\n                    </div>\n                </div>\n            </div>\n            <div class="col-5 border-right">\n                <table class="h-100 w-100">\n                    <tr>\n                        <td class="align-top">\n                            <a href="#/drive">\n                                <h5 class="mt-3 mb-0 text-danger font-weight-bold">${driveName}</h5>     \n                            </a>\n                            <div class="row">\n                                <div class="col-3">\n                                        <p class="text-muted">Leaving:</p>\n                                </div>\n                                <div class="col-9">\n                                    <p class="text-muted">${departureTime}</p>\n                                </div>\n                            </div>\n                        </td>\n                    </tr>\n                    <td class="align-bottom">\n                        <div class="row mb-0">\n                            <div class="col-3">\n                                <p class="text-muted mb-0">Pickup:</p>\n                                <p class="text-muted">Dropoff:</p>\n                            </div>\n                            <div class="col-9">\n                                <p class="text-muted mb-0">${tripStart}</p>\n                                <p class="text-muted">${tripStop}</p>\n                            </div>\n                        </div>\n                    </td>\n                </table>\n            </div>\n            <div class="col-3 border-right">\n                <table class="h-100 w-100">\n                    <tr>\n                        <td class="align-top">\n                            <h5 class="mt-3 mb-0 text-muted font-weight-bold">${carBrand} ${carModel}</h5>\n                            <p class="text-muted mb-0">${carYear} | ${carLicensePlate}</p>\n                        </td>\n                    </tr>\n                    <td class="align-bottom">\n                        <p class="text-muted mb-0">Vehicle preferences</p>\n                        <p>\n                            <i class="fas fa-suitcase fa-lg ${optLuggage}"></i>\n                            <i class="fas fa-snowflake fa-lg ${optWinterTires}"></i>\n                            <i class="fas fa-bicycle fa-lg ${optBicycle}"></i>\n                            <i class="fas fa-paw fa-lg ${optPets}"></i>\n                        </p>\n                    </td>\n                </table>\n            </div>\n            <div class="col-2">\n                <table class="h-100 w-100">\n                    <tr>\n                        <td class="align-top">\n                            <h5 id="drive-seats-left" class="mt-3 text-muted font-weight-bold">${driveNumberOfSeatsLeft} seats left</h5>\n                            ${passengerIcons}\n                        </td>\n                    </tr>\n                    <td class="align-bottom">\n                        <button class="mb-3 btn btn-danger btn-sm btn-block request-btn">Request</button>\n                    </td>\n                </table>`
+                searchResults += `<div class="row border bg-white shadow-sm">
+            <div class="col-2 border-right">
+                <h5 class="mt-3 mb-0 text-muted font-weight-bold">${driverFirstName}</h5>
+                <p class="text-muted">${driverGender}, ${driverAge}</p>
+                <div class="row mt-4">
+                    <div class="col-2">
+                        <p class="mb-0 text-muted"><i class="fas fa-car fa-sm"></i></p>
+                        <p class="text-muted mb-0"><i class="fas fa-star fa-sm"></i></p>
+                        <p class="text-muted"><i class="fas fa-chart-bar fa-sm"></i></p>
+                    </div>
+                    <div class="col-9">
+                        <p class="text-muted mb-0">${driverNumberDriven} driven</p>
+                        <p class="text-muted mb-0">${driverRating} rating</p>
+                        <p class="text-muted">${driverReviews} reviews</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-5 border-right">
+                <table class="h-100 w-100">
+                    <tr>
+                        <td class="align-top">
+                            <a href="#/drive">
+                                <h5 class="mt-3 mb-0 text-danger font-weight-bold">${driveName}</h5>     
+                            </a>
+                            <div class="row">
+                                <div class="col-3">
+                                        <p class="text-muted">Leaving:</p>
+                                </div>
+                                <div class="col-9">
+                                    <p class="text-muted">${departureTime}</p>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <td class="align-bottom">
+                        <div class="row mb-0">
+                            <div class="col-3">
+                                <p class="text-muted mb-0">Pickup:</p>
+                                <p class="text-muted">Dropoff:</p>
+                            </div>
+                            <div class="col-9">
+                                <p class="text-muted mb-0">${tripStart}</p>
+                                <p class="text-muted">${tripStop}</p>
+                            </div>
+                        </div>
+                    </td>
+                </table>
+            </div>
+            <div class="col-3 border-right">
+                <table class="h-100 w-100">
+                    <tr>
+                        <td class="align-top">
+                            <h5 class="mt-3 mb-0 text-muted font-weight-bold">${carBrand} ${carModel}</h5>
+                            <p class="text-muted mb-0">${carYear} | ${carLicensePlate}</p>
+                        </td>
+                    </tr>
+                    <td class="align-bottom">
+                        <p class="text-muted mb-0">Vehicle preferences</p>
+                        <p>
+                            <i class="fas fa-suitcase fa-lg ${optLuggage}"></i>
+                            <i class="fas fa-snowflake fa-lg ${optWinterTires}"></i>
+                            <i class="fas fa-bicycle fa-lg ${optBicycle}"></i>
+                            <i class="fas fa-paw fa-lg ${optPets}"></i>
+                        </p>
+                    </td>
+                </table>
+            </div>
+            <div class="col-2">
+                <table class="h-100 w-100">
+                    <tr>
+                        <td class="align-top">
+                            <h5 id="drive-seats-left" class="mt-3 text-muted font-weight-bold">${driveNumberOfSeatsLeft} seats left</h5>
+                            ${passengerIcons}
+                        </td>
+                    </tr>
+                    <td class="align-bottom">
+                        <button class="mb-3 btn btn-danger btn-sm btn-block request-btn">Request</button>
+                    </td>
+                </table>`
 
 
             }
