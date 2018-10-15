7787370baa71f2d77ea0fef8e64d76e38d4215d3
Mathias Kindberg <mathias.kindberg@gmail.com>
2018-10-15 17:32:45 +0200

    Added delete user and worked on warn funcionality.

--- src/main/resources/webassets/js/custom/templates/manage-users.js
+++ src/main/resources/webassets/js/custom/templates/manage-users.js
@@ -6,12 +6,70 @@ window.base.manageUsersController = (() => {
     };
 
     var view = {
+        renderCard: function(user) {
+            var t = document.getElementById('manage-users-template');
+            
+            t.content.querySelector('.admin-name').textContent = 
+                user.firstName + ' ' + user.lastName;
+            t.content.querySelector('.admin-email').textContent = 
+                user.email;
+            if (user.numberOfRatings === 0) {
+                t.content.querySelector('.admin-rating').textContent = 
+                    'No rating yet';
+            } else {
+                t.content.querySelector('.admin-rating').textContent = 
+                    user.ratingTotalScore/user.numberOfRatings + 'rating';
+            }
+            if (user.warning == null) {
+                user.warning = 0;
+            }
+            t.content.querySelector('.admin-warning').textContent = 
+                user.warning + ' warnings';
 
+            // Associate buttons and cards with correct Id
+            t.content.querySelector('.edit-profile-button').id   = 'edit-profile-' + user.userId;
+            t.content.querySelector('.give-warning-button').id   = 'give-warning-' + user.userId;
+            t.content.querySelector('.delete-account-button').id = 'delete-account-' + user.userId;
+            
+            t.content.querySelector('.manage-users-user').id = 'manage-user-card-' + user.userId;
+
+            var clone = document.importNode(t.content, true);
+            t.parentElement.appendChild(clone);
+
+            // Add functionality to buttons.
+            document.getElementById('edit-profile-' + user.userId).onclick = () =>
+                controller.editProfile(user)
+            document.getElementById('give-warning-' + user.userId).onclick = () =>
+                controller.giveWarning(user)
+            document.getElementById('delete-account-' + user.userId).onclick = () =>
+                controller.deleteAccount(user)
+        },
+        render: function() {
+            model.users.forEach(user => view.renderCard(user));
+        },
     };
 
     const controller = {
-        load: () => {
-            alert('test');
+        giveWarning: function(user) {
+            window.base.rest.warnUser(user.userId);
+        },
+        deleteAccount: function(user) {
+            window.base.rest.deleteUser(user.userId).then(function () {
+                var item = document.getElementById('manage-user-card-' + user.userId);
+                item.parentElement.removeChild(item);
+            }); 
+            
+        },
+        editProfile: function(user) {
+            console.log('EDIT!! ID: ' + user.userId);
+        },
+        load: function() {
+            base.rest.getUsers().then(function(users) {
+                model.users = users;
+                return users;
+            }).then(function() {
+                view.render();
+            });
         }
     };
 
