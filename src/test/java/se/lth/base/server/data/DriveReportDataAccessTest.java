a29475938adb53d45372ec1baf9eb6a075104889
dic15rol <robin.robbe96@gmail.com>
2018-10-15 14:54:10 +0200

    Added tests. Added missing reported_by_user_id for methods getDriveReport(int), getDriveReportsForDrive(int) in DriveReportDataAccess.
    Fixes #152

---
+++ src/test/java/se/lth/base/server/data/DriveReportDataAccessTest.java
@@ -0,0 +1,72 @@
+package se.lth.base.server.data;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import se.lth.base.server.Config;
+import se.lth.base.server.database.BaseDataAccessTest;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+public class DriveReportDataAccessTest extends BaseDataAccessTest {
+
+    private final DriveReportDataAccess driveReportDao = new DriveReportDataAccess(Config.instance().getDatabaseDriver());
+    private final DriveDataAccess driveDao = new DriveDataAccess(Config.instance().getDatabaseDriver());
+
+    private Drive drive;
+
+    @Before
+    public void createDrive() {
+        long departureTime = new Timestamp(2018 - 1900, 10, 20, 12, 0, 0, 0).getTime();
+        long arrivalTime = new Timestamp(2018 - 1900, 10, 20, 13, 0, 0, 0).getTime();
+        drive = new Drive(-1, "A", "C", departureTime, arrivalTime, "c", "x", "y", "z", "x1", 1, 1, false, false, false);
+        drive = driveDao.addDrive(drive);
+    }
+
+    @Test
+    // Tests addDriveReport(...), getDriveReportsForDrive(int), deleteDriveReport(int)
+    public void addAndDeleteDriveReport() {
+        // Check for empty list
+        List<DriveReport> driveReports = driveReportDao.getDriveReportsForDrive(drive.getDriveId());
+        Assert.assertEquals(0, driveReports.size());
+
+        // Add drive report
+        DriveReport driveReport = driveReportDao.addDriveReport(drive.getDriveId(), TEST.getId(), "Lorem ipsum");
+
+        // Check for non-empty list and validate attributes
+        driveReports = driveReportDao.getDriveReportsForDrive(drive.getDriveId());
+        Assert.assertEquals(1, driveReports.size());
+        driveReport = driveReports.get(0);
+        Assert.assertEquals(drive.getDriveId(), driveReport.getDriveId());
+        Assert.assertEquals(TEST.getId(), driveReport.getReportedByUserId());
+        Assert.assertEquals("Lorem ipsum", driveReport.getReportMessage());
+
+        // Delete drive report
+        Assert.assertEquals(true, driveReportDao.deleteDriveReport(driveReport.getReportId()));
+
+        // Check for empty list
+        driveReports = driveReportDao.getDriveReportsForDrive(drive.getDriveId());
+        Assert.assertEquals(0, driveReports.size());
+    }
+
+
+    @Test
+    // Test getDriveReport(int)
+    public void getDriveReport() {
+        // Add drive report
+        DriveReport driveReport1 = driveReportDao.addDriveReport(drive.getDriveId(), TEST.getId(), "Lorem ipsum");
+
+        // Get added drive report
+        DriveReport driveReport2 = driveReportDao.getDriveReport(driveReport1.getReportId());
+        Assert.assertNotNull(driveReport2);
+
+        // Validate attributes
+        Assert.assertEquals(driveReport1.getReportId(), driveReport2.getReportId());
+        Assert.assertEquals(driveReport1.getDriveId(), driveReport2.getDriveId());
+        Assert.assertEquals(driveReport1.getReportedByUserId(), driveReport2.getReportedByUserId());
+        Assert.assertEquals(driveReport1.getReportMessage(), driveReport2.getReportMessage());
+    }
+
+
+}
