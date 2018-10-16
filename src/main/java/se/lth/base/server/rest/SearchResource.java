5a32096b7f330f4c7d8fc8784ded792a75d00d95
Robin <robin.robbe96@gmail.com>
2018-10-16 19:00:09 +0200

    Disabled advanced drive search. Replaced old test with new test.
    Fixes #199

--- src/main/java/se/lth/base/server/rest/SearchResource.java
+++ src/main/java/se/lth/base/server/rest/SearchResource.java
@@ -65,6 +65,9 @@ public class SearchResource {
         // Get all drives matching start and end point of search
         List<Drive> filteredDrives = filterDrivesMatchingTrip(tripStart, tripStop, departureTime);
 
+        // Remove drives with too many drive users
+        removeDrivesWithTooManyDriveUsers(filteredDrives);
+
         // Turn into DriveWraps
         List<DriveWrap> driveWraps = new ArrayList<>();
         for (Drive d : filteredDrives) {
@@ -251,6 +254,18 @@ public class SearchResource {
 
         }
         return drives;
+    }
+
+    private void removeDrivesWithTooManyDriveUsers(List<Drive> drives) {
+        Iterator<Drive> iterator = drives.iterator();
+        while (iterator.hasNext()) {
+            Drive drive = iterator.next();
+            int numberOfSeats = drive.getCarNumberOfSeats();
+            int usersInDrive = driveUserDao.getNumberOfUsersInDrive(drive.getDriveId()) - 1; // Remove driver
+            if (numberOfSeats == usersInDrive) {
+                iterator.remove();
+            }
+        }
     }
 
     private List<Drive> filterDrivesMatchingTrip(String tripStart, String tripStop, Timestamp departureTime) {
