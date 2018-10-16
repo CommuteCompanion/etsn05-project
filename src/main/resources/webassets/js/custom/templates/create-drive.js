9b8e869344d63b798b556f4ac3412f0cf914faee
Arvid <arvid.pilhall@gmail.com>
2018-10-16 09:46:26 +0200

    Removed model.drive

--- src/main/resources/webassets/js/custom/templates/create-drive.js
+++ src/main/resources/webassets/js/custom/templates/create-drive.js
@@ -2,7 +2,6 @@ window.base = window.base || {};
 
 window.base.createDriveController = (() => {
     const model = {
-        drive: {},
         driveWraps: {},
         user: {},
         theId: {}
